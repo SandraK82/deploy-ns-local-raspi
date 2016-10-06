fb618a820c8fa0f930531f448c5249a4e11895da
DessertHunter <DessertHunter@gmail.com>
2016-10-06 21:58:54 +0200

    Adding Raspberry 1 Model B support
    NodeJS needs different versions for non ARMv8 (Raspi 3)
    Adding --assume-yes for apt-get install to prevent script abort due
    "additional disk space" question
    Adding install git before clone, because git is not present on Raspian
    Lite

--- Readme.md
+++ Readme.md
@@ -1,21 +1,21 @@
-__Tested with:__ 
+__Tested with:__
 
-- Raspberry Pi 3: Evrything works nicely now
+- Raspberry Pi 3: Everything works nicely now
 
 __Brief:__
 
-Use this script to setup a complete local runnign nightscout instance with a local mongoDB instance
+Use this script to setup a complete local running nightscout instance with a local mongoDB instance
 
 __Usage:__
 
 `curl -s https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/ns-local-install.sh | bash -`
 
 after running the script you will have a running nightscout local installation and an open editor with your config for nightscout. You need to configure at least the lines at tht top of the file:
-CUSTOM_TITLE=mysitename_without_spaces
-API_SECRET=my_12_character_passwort
+`CUSTOM_TITLE=mysitename_without_spaces`
+`API_SECRET=my_12_character_passwort`
 
 Put your personal password (at least 12 characters long) and teh name of your site (just for Display) there!
-Please note that you _CAN_NOT_ have any spaces in your configuration. To separate values use %20 instead. For the CUSTOM_TITLE Parameter this does not work! 
+Please note that you _CAN_NOT_ have any spaces in your configuration. To separate values use %20 instead. For the CUSTOM_TITLE Parameter this does not work!
 
 __Updates:__
 
--- ns-local-install.sh
+++ ns-local-install.sh
@@ -3,16 +3,32 @@
 ## from https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/ns-local-install.sh
 
 # make me current
-sudo apt-get update && sudo apt-get upgrade -y 
+sudo apt-get update && sudo apt-get upgrade -y
 
 # get the right node
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+CPU_MODEL=$( awk '/model name/ {print $4}' < /proc/cpuinfo )
+if [ "$CPU_MODEL" = "ARMv6-compatible" ]
+then
+  echo "ARMv6 detected"
+  # install node (on ARMv6 eg. Raspberry Model A/B/B+/A+/Zero)
+  wget https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-armv6l.tar.xz
+  tar -xvf node-v6.7.0-linux-armv6l.tar.xz
+  cd node-v6.7.0-linux-armv6l
+  sudo cp -R * /usr/local/
+  # check version should be v6.7.0
+  node -v
+  cd ..
+  # clean up
+  rm node-v6.7.0-linux-armv6l.tar.xz
+  rm -r node-v6.7.0-linux-armv6l
+else
+  # install node (on ARMv8 eg Raspberry 3 Model B)
+  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+fi
 
-# install node
-sudo apt-get install -y nodejs 
-
-# prekäre npm
-sudo apt-get install npm
+# prepare npm
+sudo apt-get install -y npm
 sudo npm cache clean -f
 sudo npm install npm -g
 sudo npm install n -g
@@ -21,11 +37,10 @@ sudo npm install n -g
 sudo n 4.5
 
 # install the mongodb 2.x from apt for now
-sudo apt-get install mongodb-server
+sudo apt-get install -y mongodb-server
 # enable mongo
 sudo systemctl enable mongodb.service
 # check mongo status
-sudo systemctl mongodb.service status
 sudo systemctl status mongodb.service
 # get log of mongo
 #cat /var/log/mongodb/mongodb.log -> should contain: [initandlisten] waiting for connections on port 27017
@@ -33,13 +48,16 @@ sudo systemctl status mongodb.service
 # go home
 cd
 # get ns
+sudo apt-get install --assume-yes git
 git clone https://github.com/nightscout/cgm-remote-monitor.git
+
+# switching to cgm-remote-monitor directory
 cd cgm-remote-monitor/
 # switch to dev
 git checkout dev
 
 # setup ns
-./setup.sh 
+./setup.sh
 
 # put your config into it
 curl -o my.env https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/my.env
